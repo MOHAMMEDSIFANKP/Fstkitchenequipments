61b5dbd6800de21f990a80a0c34c06d0a05049ac
sifan <sifan007sifu@gmail.com>
2023-12-14 21:43:52 +0530

    requiments text added

--- userside/views.py
+++ userside/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.views.generic import *
 from dashboard.models import *
+from django.http import JsonResponse
+
 # Create your views here.
 
 class Home(ListView):
@@ -14,12 +16,20 @@ class Home(ListView):
         return {'bg_images': bg_images,'category_data' : category_data,'products_data':products_data}
    
 class Category_List(ListView):
-    template_name = 'user/category_list.html'
+    template_name = 'user/category/category_list.html'
     context_object_name = 'data_list'
 
     def get_queryset(self):
         category_data = Categories.objects.all().order_by('name')
         return {'category_data' : category_data}
+    
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('query', '').strip()
+        if query:  
+            instance_data = Categories.objects.filter(name__icontains=query)
+            print(instance_data)
+            return render(request, 'user/category/search_data.html', {'category_data': instance_data})
+        return super().get(request, *args, **kwargs)
     
 class Product_List(ListView):
     template_name = 'user/products_list.html'
